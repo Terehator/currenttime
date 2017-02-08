d3bf0312b89e36b3405ed954332f608bb773b0cb
Terehator <alexeytereshko1997@gmail.com>
2017-02-08 15:07:27 +0300

    change letters in values name to lowercase back

--- src/DEV-1/Randomizer.cs
+++ src/DEV-1/Randomizer.cs
@@ -11,23 +11,23 @@ namespace DEV_1
     {
         public static void outputRandomOptions(string[] options, int numberOfNeededOptions)
         {
-            ArrayList ArrayListOfOptions = Parser.parseArrayToArrayList(options);
+            ArrayList arrayListOfOptions = Parser.parseArrayToArrayList(options);
             string result = "";
             Random random = new Random();
-            if (ArrayListOfOptions.Count >= numberOfNeededOptions)
+            if (arrayListOfOptions.Count >= numberOfNeededOptions)
             {
                 for (int i = 0; i < numberOfNeededOptions; i++)
                 {
-                    int randomIndex = random.Next(0, ArrayListOfOptions.Count);
+                    int randomIndex = random.Next(0, arrayListOfOptions.Count);
                     if (i == 0)
                     {
-                        result = result + ArrayListOfOptions[randomIndex].ToString();
+                        result = result + arrayListOfOptions[randomIndex].ToString();
                     }
                     else
                     {
-                        result = result + " " + ArrayListOfOptions[randomIndex].ToString();
+                        result = result + " " + arrayListOfOptions[randomIndex].ToString();
                     }
-                    ArrayListOfOptions.RemoveAt(randomIndex);
+                    arrayListOfOptions.RemoveAt(randomIndex);
                 }
                 Outputer.outputToConsole(result);
             }
