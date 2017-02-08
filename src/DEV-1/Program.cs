2cf5bd97447f98dab1a66a2d3c3026ef1351875e
Terehator <alexeytereshko1997@gmail.com>
2017-02-08 13:36:44 +0300

    first try

---
+++ src/DEV-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{
+    class Program
+    {
+        const int numberOfNeededOptions = 3;
+        static void Main(string[] args)
+        {
+            Randomizer.outputRandomOptions(args, numberOfNeededOptions);
+        }
+    }
+}
