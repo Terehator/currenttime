fea8140f001ffdb9552a4368416b536361073938
Terehator <alexeytereshko1997@gmail.com>
2017-02-08 14:34:37 +0300

    Update Program.cs
    add comments

--- src/DEV-1/Program.cs
+++ src/DEV-1/Program.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace DEV_1
 {
+    //Main class
     class Program
     {
         const int numberOfNeededOptions = 3;
         static void Main(string[] args)
         {
-            Randomizer.outputRandomOptions(args, numberOfNeededOptions);
+            Randomizer.outputRandomOptions(args, numberOfNeededOptions); //Output to console needed number of random options from args
         }
     }
 }
