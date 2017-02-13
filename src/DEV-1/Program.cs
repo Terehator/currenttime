8fc821b639cfbba8a86d48f2ded80324f1d33aa0
Terehator <alexeytereshko1997@gmail.com>
2017-02-13 14:55:55 +0300

    add other files back

---
+++ src/DEV-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{
+    class Program
+    {
+        const int numberOfNeededOptions = 3;
+        static void Main(string[] args) //Main class
+        {
+            Randomizer randomizer = new Randomizer(numberOfNeededOptions);
+            randomizer.outputRandomOptions(args); //Output needed number of options into the console
+        }
+    }
+}
