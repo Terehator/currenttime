8a73cb77d736f973f7f856ca0c7939edda5dd055
Terehator <alexeytereshko1997@gmail.com>
2017-02-15 14:44:58 +0300

    change codestyle

--- src/DEV-3/DEV-3/Program.cs
+++ src/DEV-3/DEV-3/Program.cs
@@ -17,23 +17,31 @@ namespace DEV_3
             AverageAgeCounter counter = new AverageAgeCounter();
             Console.WriteLine("Average age: " + counter.countAverageAge(usersList));
         }
+        
+        
         private static void printOldestUsers(List<User> usersList, UserOutputer userOutputer)
         {
             ElderUserCounter elderUserCounter = new ElderUserCounter();
             Console.WriteLine("User with max age:");
-            userOutputer.outputListOfUsers(elderUserCounter.getOldestUser(usersList)); //Output users with max age
+            userOutputer.outputListOfUsers(elderUserCounter.getOldestUser(usersList)); // Output users with max age
         }
+        
+        
         private static void printMostPopularWomanName(List<User> usersList)
         {
             MostPopularWomanNameFinder mostPopularWomanNameFinder = new MostPopularWomanNameFinder();
             Console.WriteLine("Most popular woman name: " + mostPopularWomanNameFinder.findMostPopularWomanName(usersList));
         }
+        
+        
         private static void printHomonyms(List<User> usersList, UserOutputer userOutputer)
         {
             HomonymFinder homonymFinder = new HomonymFinder();
             Console.WriteLine("Homonyms: ");
             userOutputer.outputListOfUsers(homonymFinder.findHomonyUsers(usersList));
         }
+        
+        
         static void Main(string[] args)
         {
             UserInputer userInputer = new UserInputer();
