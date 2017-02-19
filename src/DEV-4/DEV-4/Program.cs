9b2004f15e40c6754652c70cd93f82ea63b4cb5d
Terehator <alexeytereshko1997@gmail.com>
2017-02-19 16:29:30 +0300

    first try

---
+++ src/DEV-4/DEV-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_4
+{
+    /// <summary>
+    /// Main class
+    /// Show functionality
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextParser testParser = new TextParser();
+            string text = testParser.TextParse(Console.ReadLine());
+            FrequencyCalculator frequencyCalculator = new FrequencyCalculator();
+            frequencyCalculator.CalculateFrequencies(text);
+            Console.ReadKey();
+            WordSorter wordSorter = new WordSorter();
+            wordSorter.PrintWordsOfNeededLength(text);
+        }
+    }
+}
---
+++ src/DEV-4/DEV-4/WordSorter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_4
+{
+    class WordSorter
+    {
+        private int LENGTH = 5;
+
+
+        public void PrintWordsOfNeededLength(string text)
+        {
+            List<string> listOfWords = text.Split(' ').ToList();
+            foreach (string word in listOfWords)
+            {
+                if (word.Length >= LENGTH)
+                    Console.WriteLine(word);
+            }
+        }
+    }
+}
