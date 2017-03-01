26bce0850986ee219cab4d0ae8fefad7ff3d46d1
Terehator <alexeytereshko1997@gmail.com>
2017-03-01 14:44:50 +0300

    DEV-7 architecture builded

---
+++ src/DEV-7/DEV-7/Time.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_6
+{
+    class Time
+    {
+        public int hours { get; set; }
+        public int seconds { get; set; }
+        public int minutes { get; set; }
+        public Time(int hours, int minutes, int seconds)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+    }
+}
---
+++ src/DEV-7/DEV-7/TimeValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_6
+{
+    class TimeValidator
+    {
+        int HOUR_MAX = 23;
+        int MINUTES_MAX = 60;
+        int SECONDS_MAX = 60;
+        int HOUR_MIN = 0;
+        int MINUTES_MIN = 0;
+        int SECONDS_MIN = 0;
+        public bool Validate(Time time)
+        {
+            return (time.hours >= HOUR_MIN && time.hours <= HOUR_MAX &&
+                    time.minutes >= MINUTES_MIN && time.minutes <= MINUTES_MAX &&
+                    time.seconds >= SECONDS_MIN && time.seconds <= SECONDS_MAX);
+        }
+    }
+}
