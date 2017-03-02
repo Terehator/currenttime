2caad178cfc18d2c0b5ef85e181f3a699a7aad23
Terehator <alexeytereshko1997@gmail.com>
2017-03-02 16:10:13 +0300

    DEV-8 is complimented

---
+++ src/DEV-8/DEV-8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatrixInputer matrixInputer = new MatrixInputer();
+            MatrixMultiplier matrixMultiplier = new MatrixMultiplier();
+            double[,] matrix1 = matrixInputer.InputFromConsole();
+            double[,] matrix2 = matrixInputer.InputFromConsole();
+            double[,] result;
+            while (!matrixMultiplier.TryMultiply(matrix1, matrix2, out result))
+            {
+                matrix1 = matrixInputer.InputFromConsole();
+                matrix2 = matrixInputer.InputFromConsole();
+            }
+            MatrixOutputer matrixOutputer = new MatrixOutputer();
+            matrixOutputer.OutputToConsole(result);
+            Console.ReadKey();
+        }
+    }
+}
