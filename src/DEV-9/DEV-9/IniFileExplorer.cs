f2004144ff4a84a70c1bfc28a9aec269b9196119
Terehator <alexeytereshko1997@gmail.com>
2017-03-03 12:19:05 +0300

    DEV-9 is completed

---
+++ src/DEV-9/DEV-9/IniFileExplorer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DEV_9
+{
+    /// <summary>
+    /// Explore .ini file
+    /// </summary>
+    class IniFileExplorer
+    {
+        string path;
+        public IniFileExplorer(string path)
+        {
+            this.path = path;
+        }
+        /// <summary>
+        /// Get value at section and key
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetValueBySectionAndKey(string section, string key)
+        {
+            using (StreamReader reader = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string sectionPattern = string.Concat(@"^\[", section, @"\]$");
+                string line = reader.ReadLine();
+                while (!Regex.IsMatch(line, sectionPattern) && !reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                }
+                if (!Regex.IsMatch(line, sectionPattern) && reader.EndOfStream)
+                    throw new SectionNotFoundException(string.Concat("Can't find section ", section));
+                else
+                {
+                    return GetValueByKeyInCurrentSection(section, key, reader);
+                }
+            }
+        }
+        /// <summary>
+        /// Get value by key only in one section
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private string GetValueByKeyInCurrentSection(string section, string key, StreamReader reader)
+        {
+            string keyPattern = string.Concat("^", key, "=");
+            string sectionPattern = @"^\[.*\]$";
+            string line = reader.ReadLine();
+            while (!Regex.IsMatch(line, keyPattern)
+                && !Regex.IsMatch(line, sectionPattern)
+                && !reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+            }
+            if (!Regex.IsMatch(line, keyPattern))
+                throw new KeyNotFoundException(string.Concat("Can't find key ", key, " in section ", section));
+            else
+            {
+                return Regex.Replace(line, keyPattern, string.Empty);
+            }
+        }
+    }
+}
---
+++ src/DEV-9/DEV-9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IniFileExplorer iniExplorer;
+            Inputer inputer = new Inputer();
+            if (args.Length != 0) 
+                iniExplorer = new IniFileExplorer(args[0]);
+            else
+                iniExplorer = new IniFileExplorer(inputer.InputUntillNotEmpty("Enter filepath"));
+            string section = inputer.InputUntillNotEmpty("Enter section");
+            string key = inputer.InputUntillNotEmpty("Enter key");
+            try
+            {
+                Console.WriteLine(iniExplorer.GetValueBySectionAndKey(section, key));
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Incorrect name of file");
+            }
+            catch (SectionNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
