7a9ca5f0366da3cfb3651e841cd80f6da047f994
Terehator <alexeytereshko1997@gmail.com>
2017-03-10 12:44:42 +0300

    reading is completed

---
+++ src/DEV-10/DEV-10/Product.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_10
+{
+    class Product
+    {
+        private int productID;
+        public int ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
+        private string productName;
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+    }
+}
